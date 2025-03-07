2dd749522890211788358ef2253730a2a5cbbc95
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-03-07 17:46:28 -0500

    Hard-coded short description

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ import setuptools
 _ENCODING_UTF8 = "utf-8"
 _MODE_R = "r"
 
-_NEW_LINE = "\n"
 
-
-def _make_descriptions():
+def _make_long_description():
 	with open("README.md", _MODE_R, encoding=_ENCODING_UTF8) as readme_file:
 		readme_content = readme_file.read()
 
@@ -22,19 +20,15 @@ def _make_descriptions():
 	index_desc_end_en = readme_content.index("### Content", index_desc_en)
 	index_end_en = readme_content.index("### Dependencies", index_desc_end_en)
 
-	short_description = readme_content[index_desc_en: index_desc_end_en]
-	short_description = short_description.replace(_NEW_LINE, " ")
-	short_description = short_description.replace("`", "")
-	short_description = short_description.strip()
-
 	long_description = readme_content[index_fr: index_end_fr]\
 		+ readme_content[index_en:index_end_en].strip()
 
-	return short_description, long_description
+	return long_description
 
 
 if __name__ == "__main__":
-	short_desc, long_desc = _make_descriptions()
+	long_desc = _make_long_description()
+	short_desc = "Exception GitHubApiError can be raised when a request to the GitHub API fails."
 
 	setuptools.setup(
 		name = "ghae",
@@ -45,7 +39,7 @@ if __name__ == "__main__":
 		long_description_content_type = "text/markdown",
 		url = "https://github.com/GRV96/ghae",
 		classifiers = [
-			"Private :: Do Not Upload", # Temporarily prevent the publication.
+			"Private :: Do Not Upload", # Remove when ready to publish.
 			"Development Status :: 5 - Production/Stable",
 			"Intended Audience :: Developers",
 			"License :: OSI Approved :: MIT License",
