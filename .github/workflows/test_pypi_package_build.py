59a3a1bbf4d5c02ae01bcdd5018b4f9790887c21
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2025-03-07 22:49:26 -0500

    CI workflow (#3)
    The workflow runs the automated tests and ensures that the package is
    correctly built.

---
+++ .github/workflows/test_pypi_package_build.py
@@ -0,0 +1,18 @@
+"""
+This script is meant to run on GitHub's virtual machine ubuntu-latest in a CI
+workflow. It builds the PyPI package and installs it to ensure that the package
+works. However, the script does not upload the package to PyPI.
+"""
+
+
+from os import system
+from pathlib import Path
+
+
+repo_root = Path(__file__).resolve().parents[2]
+
+system(f"python3 {repo_root}/setup.py sdist")
+
+src_dist = next((repo_root/"dist").glob("ghae-*.tar.gz"))
+
+system(f"pip3 install --no-cache-dir {src_dist}")
